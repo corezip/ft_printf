e798edc1a3837d46e578e3432081c0bdffcd6f90
Gerardo Solis <gsolis@e1z2r4p11.42.us.org>
2017-04-06 22:59:07 -0700

    printf

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 
 int			main(void)
 {
-	// char s[100] = "alkfjhalkjsdfhlkajshdflkjahsldkjfhalksjdhflkajhsdflkjhalskdj";
-	printf("OR: |%#X\n", -15656546546);
-	ft_printf("FT: |%#X\n", -15656546546);
-	// printf("OR: |%p\n", s);  6565654654654
-	// ft_printf("FT: |%p\n", s);
+	char s[100] = "alkfjhalkjsdfhlkajshdflkjahsldkjfhalksjdhflkajhsdflkjhalskdj";
+	// printf("OR: |%#X\n", -15656546546);
+	// ft_printf("FT: |%#X\n", -15656546546);
+	printf("OR: |%p\n", s);
+	ft_printf("FT: |%p\n", s);
+	// 6565654654654
 	return (0);
 }
